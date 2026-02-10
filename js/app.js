6ec746105c002e83b6d3308d9b2452c2a9f6abc9
dado053 <dimpap053@gmail.com>
2026-02-10 21:05:33 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,87 @@
+// Elements
+const envelope = document.getElementById("envelope-container");
+const letter = document.getElementById("letter-container");
+const noBtn = document.querySelector(".no-btn");
+const yesBtn = document.querySelector(".btn[alt='yes']");
+
+const title = document.getElementById("letter-title");
+const catImg = document.getElementById("letter-cat");
+const buttons = document.getElementById("letter-buttons");
+const finalText = document.getElementById("final-text");
+
+function playAudio(){
+  const audio = document.getElementById("audio");
+  audio.volume = 0;
+  audio.play();
+  let vol = 0;
+  const fade = setInterval(() => {
+    vol += 0.02;
+    if(vol >= 0.4){
+      vol = 1;
+      clearInterval(fade);
+    }
+    audio.volume = vol;
+  }, 100);
+}
+
+// Click Envelope
+
+envelope.addEventListener("click", () => {
+  envelope.style.display = "none";
+  letter.style.display = "flex";
+  playAudio();
+  setTimeout( () => {
+    document.querySelector(".letter-window").classList.add("open");
+  },50);
+});
+
+// Logic to move the NO btn
+
+noBtn.addEventListener("mouseover", () => {
+  const min = 200;
+  const max = 200;
+
+  const distance = Math.random() * (max - min) + min;
+  const angle = Math.random() * Math.PI * 2;
+
+  const moveX = Math.cos(angle) * distance;
+  const moveY = Math.sin(angle) * distance;
+
+  noBtn.style.transition = "transform 0.3s ease";
+  noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
+});
+
+// Logic to make YES btn to grow
+
+let yesScale = 1;
+
+yesBtn.style.position = "relative"
+yesBtn.style.transformOrigin = "center center";
+yesBtn.style.transition = "transform 0.3s ease";
+
+noBtn.addEventListener("mouseover", () => {
+yesScale += 0.3;
+
+      if (yesBtn.style.position !== "fixed") {
+         yesBtn.style.position = "fixed";
+         yesBtn.style.top = "50%";
+         yesBtn.style.left = "50%";
+         yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
+     }else{
+         yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
+     }
+ });
+
+// YES is clicked
+
+yesBtn.addEventListener("click", () => {
+  title.textContent = "❤️❤️❤️❤️ΣΕ ΑΓΑΠΑΩ ΓΛΥΚΙΑ ΜΟΥ❤️❤️❤️❤️";
+
+  catImg.src = "img/cat_dance.gif";
+
+  document.querySelector(".letter-window").classList.add("final");
+
+  buttons.style.display = "none";
+
+  finalText.style.display = "block";
+});
